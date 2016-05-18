1633135dbb1532f5d460af8988d253a05b382d0e
Thomas Scott <thomas.scott@squidsolutions.com>
2016-05-18 14:04:23 +0200

    T1244

--- src/squid_api_modelinfo_widget.js
+++ src/squid_api_modelinfo_widget.js
@@ -7,27 +7,100 @@
 
         template: template,
         popoverOptions: {
-            placement: "left",
-            html: true
+            placement: function (context, source) {
+                var position = $(source).position();
+
+                if (position.left > 515) {
+                    return "left";
+                }
+
+                if (position.left < 515) {
+                    return "right";
+                }
+
+                if (position.top < 110){
+                    return "bottom";
+                }
+
+                return "top";
+            },
+            html: true,
+            container: 'body'
         },
         internalTemplate: null,
 
         initialize: function() {
+            this.config = squid_api.model.config;
             this.internalTemplate = squid_api.template.squid_api_modelinfo_internal_widget;
-            this.status = squid_api.model.status;
-            this.status.on("change:configReady", this.getContent, this);
+
+            this.config.on("change:domain", this.render, this);
         },
 
-        getContent: function() {
-            this.popoverOptions.content = this.internalTemplate();
-            this.render();
+        getDimensions: function() {
+            var me = this;
+            return squid_api.getCustomer().then(function(customer) {
+                return customer.get("projects").load(me.config.get("project")).then(function(project) {
+                    return project.get("domains").load(me.config.get("domain")).then(function(domain) {
+                        return domain.get("dimensions").load();
+                    });
+                });
+            });
+        },
+
+        getMetrics: function() {
+            var me = this;
+            return squid_api.getCustomer().then(function(customer) {
+                return customer.get("projects").load(me.config.get("project")).then(function(project) {
+                    return project.get("domains").load(me.config.get("domain")).then(function(domain) {
+                        return domain.get("metrics").load();
+                    });
+                });
+            });
         },
 
         render: function() {
-            // print base template
-            this.$el.html(this.template());
-            // initialize popover
-            this.$el.find("[data-toggle='popover']").popover(this.popoverOptions);
+            var me = this;
+            var domain = this.config.get("domain");
+
+            if (domain) {
+                // print base template
+                this.$el.html(this.template());
+
+                // get domain dimensions & metrics
+                $.when( this.getDimensions(), this.getMetrics() ).done(function ( dimensions, metrics ) {
+                    var jsonData = {
+                        "dimensions": [],
+                        "metrics": []
+                    };
+
+                    // store dimensions
+                    for (var d=0; d<dimensions.length; d++) {
+                        jsonData.dimensions.push({
+                            "name": dimensions.at(d).get("name"),
+                            "description": dimensions.at(d).get("description")
+                        });
+                    };
+
+                    // store metrics
+                    for (var m=0; m<metrics.length; m++) {
+                        jsonData.metrics.push({
+                            "name": metrics.at(m).get("name"),
+                            "description": metrics.at(m).get("description")
+                        });
+                    };
+
+                    // set popup html content
+                    me.popoverOptions.content = me.internalTemplate(jsonData);
+
+                    // initialize popover
+                    me.$el.find("[data-toggle='popover']").popover(me.popoverOptions);
+
+                    // remove max-width
+                    me.$el.find("[data-toggle='popover']").on("show.bs.popover", function(e){
+                        me.$el.find("[data-toggle='popover']").data("bs.popover").tip().css({"max-width": "inherit"});
+                    });
+                });
+            }
 
             return this;
         }
