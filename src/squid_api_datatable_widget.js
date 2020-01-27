ccf915e9e02fb3568906d0fb94dfc688de397bd4
Dimitris Moulas <d.moulas@atc.gr>
2020-01-27 09:40:45 +0200

    remove totals from DR_D1 & PR_P1

--- src/squid_api_datatable_widget.js
+++ src/squid_api_datatable_widget.js
@@ -101,6 +101,10 @@
 				this.filters = squid_api.model.filters;
 			}
 
+			if(options.removeTotals){
+				this.removeTotals = options.removeTotals;
+			}
+
 			if (options.maxRowsPerPage) {
 				this.maxRowsPerPage = options.maxRowsPerPage;
 			}
@@ -862,6 +866,10 @@
 				});
 
 				if (results.totalSize>0) {
+					// Hide totals for PR_P1 & DR_D1 reports
+					if( this.removeTotals && (this.config.get('bookmark') === 'pr_p1' || this.config.get('bookmark') === 'dr_d1')){
+						$('tr.group td.new-category.current.measure').text('');
+					}
 					// display total
 					this.$el.find("#no-data").hide();
 					var resultsInfo = "Showing "+(results.startIndex + 1) + " - " + (results.startIndex + data.results.rows.length)+" of "+results.totalSize+" entries";
